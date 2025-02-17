2505220c41b890c1ace8abe9f26a1d3aa42819ad
Lucas GILHARD <lucas@gilhard.fr>
2025-02-17 15:03:46 +0100

    chore(eslint): disable vue/no-multiple-template-root rule

--- templates/minimal-shadcn/eslint.config.ts
+++ templates/minimal-shadcn/eslint.config.ts
@@ -2,6 +2,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off'
   }
 })
